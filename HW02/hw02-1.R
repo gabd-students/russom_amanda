fc39da70d26c9ca4b8b6e11b2ac2a8b5da99825d
Amanda Russom <amanda.russom@psd.local>
2020-07-01 11:59:10 -0500

    beginning  part 2

---
+++ HW02/hw02-1.R
@@ -0,0 +1,114 @@
+# Amanda Russom
+
+# HW02 Part 1: Intro to Basics
+
+# Insert unorginal comment. 
+
+# Add 30 + 12
+
+30 + 12
+
+# Section 1.2
+
+# Division
+
+121/1.1
+
+# Exponentiation
+
+2^5
+
+# Modulo
+
+28 %% 6
+
+# Exponentiation without parentheses
+
+49^1/2
+
+# Exponentiation with parentheses 
+
+49^(1/2)
+
+# 1.3 Variable assignment
+
+# assign the value 42 to x
+ 
+ x <-42
+
+# print out the value of variable x
+
+ x
+
+ # Assign the value of 24 to the variable d_merriami 
+ 
+ d_merriami <-24
+ 
+ # assgign the value of 9 to d_ordii
+ 
+ d_ordii <- 9
+ 
+ # check results 
+ 
+ d_merriami
+ 
+ d_ordii
+ 
+ ### 1.4 working with variables 
+ 
+ # correcting the mistake
+ 
+ d_merriami + 5
+
+ d_merriami
+
+ # correcting the mistake 
+ 
+ d_merriami <- d_merriami + 5
+
+ d_merriami
+ 
+ 
+ # add together the number of individuals of each species
+ 
+ # store the result in total_k_rate
+ 
+ total_k_rats <- d_merriami + d_ordii
+ 
+ # assign 1000 to a variable called area_sampled
+ 
+ area_sampled <- 1000
+
+ # divide total_k_rats by area_sampled to calculate density
+ 
+ density <- total_k_rats / area_sampled
+
+ # show the value of density 
+ 
+ density
+
+ # assign 42 numeric_var
+ 
+ numeric_var <- 42
+
+ # assign 'Dipodomys merriami' to species_name
+ 
+ species_name <- 'Dipodomys merriami'
+
+ # Assign TRUE to is_mammal
+ 
+ is_mammal <- TRUE
+
+ 
+# check class of numeric_var
+ 
+ class(numeric_var)
+
+ # check class of species_name
+ 
+ class(species_name)
+ 
+ # check class of is_mammal
+ 
+ class(is_mammal)
+ 
