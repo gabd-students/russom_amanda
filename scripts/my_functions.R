98605f95fba20bb04c8e78e66e2fc7b162707ed1
Amanda Russom <amanda.russom@psd.local>
2020-07-22 11:56:55 -0500

    Corrected functions

--- scripts/my_functions.R
+++ scripts/my_functions.R
@@ -2,19 +2,21 @@
 ## Calculating the standard error of the mean
 ##The first argument tells whether to remove NA, defaults to FALSE
 
-std_err <- function(x){sqrt(var(x)/length(x))}
+std_err <- function(x, na.rm=FALSE){sqrt(var(x)/length(x))}
+
+
 
 ### Scaled mass index function....maybe
 
-scaled_mass <- function(mass = 0, tarsus = 0, slope = 0) {mass*((mean(tarus))/tarus)^slope}
+scaled_mass <- function(mass = 0, tarsus = 0, slope = 0) {mass*((mean(tarsus))/tarsus)^slope}
 
 ### mass vector
 
 mass <- c(26.7, 22.8, 25.7, 26.1, 23.9)
 
-### tarus vectors
+### tarsus vectors
 
-tarus <- c(18.4, 17.5, 18.4, 18.0, 18.2)
+tarsus <- c(18.4, 17.5, 18.4, 18.0, 18.2)
 
 ### assign slope value
 
