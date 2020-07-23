04942dc0b36c7dc63ae989558f4f9b73627fb18a
Amanda Russom <amanda.russom@psd.local>
2020-07-23 16:23:19 -0500

    Working on feathers data

--- scripts/my_functions.R
+++ scripts/my_functions.R
@@ -1,13 +1,14 @@
 ### Standard error function
 ## Calculating the standard error of the mean
+
+
 ##The first argument tells whether to remove NA, defaults to FALSE
 
-
-std_err <- function(x, na.rm =FALSE) { 
-  ifelse (na.rm,                       
-          x <- na.omit(x), 
-          x)
-  sd(x)/ sqrt(length(x))
+std_err <- function(values, na.rm = FALSE) {
+  ifelse(na.rm,                           # using ifelse() because we have 2 values
+         values <- na.omit(values),       # If NA is TRUE
+         values)                          # If NA is FALSE
+  (sqrt(var(values)/length(values)))                # Standard error formula
 }
 
 
